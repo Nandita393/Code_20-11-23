0f63ae11431384d4462181f854ea2c0e61279561
Nandita393 <74349058+Nandita393@users.noreply.github.com>
2023-11-21 07:41:23 +0530

    Compoundinterest.java

--- Compoundinterest.java
+++ Compoundinterest.java
@@ -2,42 +2,41 @@ package code20;
 import java.util.Scanner;
 public class Compoundinterest {
 
-	
-
-	
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
-
-	        // Get user input for principal amount, rate of interest, and time
-	        System.out.print("Enter the principal amount: ");
+		    
+	        System.out.print("Enter the principal amount here : ");
 	        double principal = scanner.nextDouble();
 
-	        System.out.print("Enter the rate of interest (in percentage): ");
+	        System.out.print("Enter the rate of interest in percentage: ");
 	        double rateOfInterest = scanner.nextDouble();
 
-	        System.out.print("Enter the time (in years): ");
-	        double time = scanner.nextDouble();
+	        System.out.print("Enter the years: ");
+	        double years = scanner.nextDouble();
 
-	        // Calculate simple interest and compound interest
-	        double simpleInterest = calculateSimpleInterest(principal, rateOfInterest, time);
-	        double compoundInterest = calculateCompoundInterest(principal, rateOfInterest, time);
+	        
+	        double simpleInterest = calculateSimpleInterest(principal, rateOfInterest, years);
+	        double compoundInterest = calculateCompoundInterest(principal, rateOfInterest, years);
 
-	        // Display the results
-	        System.out.println("Simple Interest: " + simpleInterest);
-	        System.out.println("Compound Interest: " + compoundInterest);
+	       
+	        System.out.println("Simple Interest is : " + simpleInterest);
+	        System.out.println("Compound Interest is : " + compoundInterest);
 
-	        // Close the scanner
+	        
 	        scanner.close();
 	    }
 
 	    // Function to calculate simple interest
-	    private static double calculateSimpleInterest(double principal, double rateOfInterest, double time) {
-	        return (principal * rateOfInterest * time) / 100;
+	    private static double calculateSimpleInterest(double principal, double rateOfInterest, double years)
+	{
+	        return (principal * rateOfInterest * years) / 100;
 	    }
 
 	    // Function to calculate compound interest
-	    private static double calculateCompoundInterest(double principal, double rateOfInterest, double time) {
-	        double compoundInterest = principal * Math.pow((1 + rateOfInterest / 100), time) - principal;
+	    private static double calculateCompoundInterest(double principal, double rateOfInterest, double years)
+	{
+	        double compoundInterest = principal * Math.pow((1 + rateOfInterest / 100), years) - principal;
 	        return compoundInterest;
 	    }
 	}
+
